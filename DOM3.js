10c693a5d8c2dc2789edcab3652b35da4cb09df5
gaddechiranjeevi <gaddechiru@gmail.com>
2022-09-29 08:04:35 +0530

    filter

--- DOM3.js
+++ DOM3.js
@@ -1,6 +1,10 @@
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+var form = document.getElementById('addform');
 
 itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
 function removeItem(e){
     if(e.target.classList.contains('delete')){
         if(confirm('Are u sure?')){
@@ -8,4 +12,19 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+function filterItems(e){
+
+    var text = e.taget.value.toLowerCase();
+
+    var item = e.target.value.toLowerCase();
+
+    Array.from(item).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    })
 }
